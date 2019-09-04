f14694910b32ab8cb25ed8bbbbe02d1270313d9d
GamerNoTitle <bili33@87ouo.top>
2019-09-04 13:35:29 +0800

    completed 1024 (timed out)

---
+++ oj.noi.cn/1024/Project.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    long long a,result=0;
+	cin>>a;
+	for (long long i=1;i<=a;i++)
+	{
+		long long x;
+		x=a%i;
+		if (x==0)
+		{
+			result++;
+		}	
+	 } 
+	 cout<<result;
+}
